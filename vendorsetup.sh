3698c099746679dce42e0e84cd75da50efff02f1
Sushrut1101 <guptasushrut@gmail.com>
2023-09-23 22:21:01 +0530

    everpal: Add OrangeFox build variables
    Change-Id: Ic38f84ed5e0bd44528cc6a81be91149b53d0734b
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,91 @@
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+FDEVICE="everpal"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+	if [ -n "$chkdev" ]; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+	fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export FOX_VANILLA_BUILD=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_VIRTUAL_AB_DEVICE=1
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_NANO_EDITOR=1
+    	export FOX_DELETE_AROMAFM=1
+    	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+
+	# Screen Settings
+	export OF_SCREEN_H=2400
+	export OF_STATUS_H=100
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+  	export OF_HIDE_NOTCH=1
+	export OF_CLOCK_POS=1
+
+	# MediaTek
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+
+	# Asserts
+	export FOX_TARGET_DEVICES="evergo,evergreen,everpal,opal"
+	export TARGET_DEVICE_ALT="evergo,evergreen,opal"
+
+	# R11.1 Settings
+	export FOX_VERSION="R11.1"
+	export OF_MAINTAINER="Sushrut1101"
+
+	# Magisk
+	function download_magisk(){
+		# Usage: download_magisk <destination_path>
+		local DEST=$1
+		if [ -n "${DEST}" ]; then
+			if [ ! -e ${DEST} ]; then
+				echo "Downloading the Latest Release of Magisk..."
+				local LATEST_MAGISK_URL=$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep "browser_download_url" | grep "Magisk." | cut -d : -f 2,3 | tr -d '"')
+				mkdir -p $(dirname ${DEST})
+				wget -q ${LATEST_MAGISK_URL} -O ${DEST} || wget ${LATEST_MAGISK_URL} -O ${DEST}
+				local RCODE=$?
+				if [ "$RCODE" = "0" ]; then
+					echo "Successfully Downloaded Magisk to ${DEST}!"
+					echo "Done!"
+				else
+					echo "Failed to Download Magisk to ${DEST}!"
+				fi
+			fi
+		fi
+	}
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
+	download_magisk $FOX_USE_SPECIFIC_MAGISK_ZIP
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	fi
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
+fi
